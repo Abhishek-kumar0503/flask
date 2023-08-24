903a0c898d995720cd518b356e1acdcbb79dde42
Abhishek Kumar <97353002+Abhishek-kumar0503@users.noreply.github.com>
2023-08-24 12:55:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
 import requests
+import re
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -8,25 +8,33 @@ app = Flask(__name__)
 bot_token = "6376673556:AAFUw21pWQ3vPv1BZLDtl2_Xf23iMEmYFLA"
 base_url = f"https://api.telegram.org/bot{bot_token}/"
 
+
 def get_chat_id(update):
     return update["message"]["chat"]["id"]
 
-def is_valid_url(url):
-    return re.match(r'^(http|https)://', url) is not None
+def read_msg(offset):
+    parameter = {
+        "offset": offset
+    }
+    resp = requests.get(base_url + "getUpdates", params=parameter)
+    data = resp.json()
 
-def read_msg(update):
-    message_text = update.get("message", {}).get("text")
-    chat_id = get_chat_id(update)
-    if message_text and is_valid_url(message_text):
-        response = requests.get(message_text)
-        message = response.url
-        recording_id = re.search(r'recordingId=(\d+)', message)
-        if recording_id:
-            v = recording_id.group(1)
-            video_url = f"https://static.smpopular.com/production/uploading/recordings/{v}/master.mp4"
-            send_video(chat_id, video_url)
-    else:
-        send_msg(chat_id, "Invalid or missing recording ID. Please provide a valid link.")
+    for result in data["result"]:
+        message = result.get("message", {}).get("text")
+        chat_id = get_chat_id(result)
+        if message and is_valid_url(message):
+            response = requests.get(message)
+            message_text = response.url
+            recording_id = re.search(r'recordingId=(\d+)', message_text)
+            if recording_id:
+                v = recording_id.group(1)
+                video_url = f"https://static.smpopular.com/production/uploading/recordings/{v}/master.mp4"
+                send_video(chat_id, video_url)
+        else:
+            send_msg(chat_id, "Invalid or missing recording ID. Please provide a valid link.")
+
+    if "result" in data and data["result"]:
+        return data["result"][-1]["update_id"] + 1
 
 def send_msg(chat_id, text):
     if chat_id:
@@ -34,19 +42,22 @@ def send_msg(chat_id, text):
             "chat_id": chat_id,
             "text": text if text else "This URL will not exist. Please check it."
         }
-        requests.post(base_url + "/sendMessage", data=parameter)
+        requests.post(base_url + "sendMessage", data=parameter)
 
 def send_video(chat_id, video_url):
     if chat_id:
         parameter = {
-            "chat_id": chat_id
+            "chat_id": chat_id,
         }
         files = {
             "video": requests.get(video_url).content
         }
-        requests.post(base_url + "/sendVideo", data=parameter, files=files)
-        
-@app.route("/webhook", methods=["POST","GET"])
+        resp = requests.post(base_url + "sendVideo", data=parameter, files=files)
+
+def is_valid_url(url):
+    return re.match(r'^https?://', url) is not None
+
+@app.route("/webhook", methods=["POST"])
 def webhook():
     if request.method == "POST":
         update = request.json
